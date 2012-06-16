166f07a08ce8a8876e30a0f6f8f422fa78f8d5ad
Simon <simon@hotmail.com>
2012-06-16 13:06:23 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeshesOfCinema::Application.config.secret_token = 'f9a0d839058a25ad4cebec0bec894880292527b03f655e5e828ae5050f3b24e6685f9cf2b2e42ce27c3ea6533e254bd2b8da831c401a1b5ba0d47abfa0d2acfa'
